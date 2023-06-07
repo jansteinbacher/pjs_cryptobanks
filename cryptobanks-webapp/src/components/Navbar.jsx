3a4acd1ecb6fbdcc37834899229eab2996d988c6
jansteinbacher <jan.steinbacher@stud-mail.uni-wuerzburg.de>
2023-06-07 09:59:32 +0200

    hero section update

--- cryptobanks-webapp/src/components/Navbar.jsx
+++ cryptobanks-webapp/src/components/Navbar.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
+
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center  p-4 mb-12">
+    <nav className="flex justify-between items-center p-4 mb-12">
       <div className="text-xl">Logo</div>
       <div className="flex items-center space-x-12 mr-12">
-        <div className="hover:text-gray-800 hover:underline">About</div>
-        <div className="hover:text-gray-800 hover:underline">Einstieg</div>
-        <div className="hover:text-gray-800 hover:underline">Anwendung, Geschäftsmodelle &amp; Fallstudien</div>
+        <div className="relative hover:text-gray-800">
+          <span className="hover-underline">About</span>
+        </div>
+        <div className="relative hover:text-gray-800">
+          <span className="hover-underline">Einstieg</span>
+        </div>
+        <div className="relative hover:text-gray-800">
+          <span className="hover-underline">Anwendung, Geschäftsmodelle &amp; Fallstudien</span>
+        </div>
       </div>
     </nav>
   );
