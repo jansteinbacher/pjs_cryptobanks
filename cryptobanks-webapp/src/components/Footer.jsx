88d94bfbb607a1e8e3d9da96e79eff03c0f67a31
jansteinbacher <jan.steinbacher@stud-mail.uni-wuerzburg.de>
2023-06-09 13:36:53 +0200

    navbar update

--- cryptobanks-webapp/src/components/Footer.jsx
+++ cryptobanks-webapp/src/components/Footer.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import Crypto from '../images/crypto.png';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 py-4">
+    <footer className="bg-emerald-100 py-4">
       <div className="container mx-auto px-4 flex items-center justify-between">
-        <div className="flex items-center">
-          <span className="text-white">Cryptobanks</span>
+      <a href='/'>
+        <div className="text-xl flex items-center">
+          <img className="w-12 h-auto mr-4" src={Crypto} alt="Crypto" />
+          <span>Cryptobanks</span>
         </div>
+      </a>
         <a href='/'>
-        <div className="text-gray-300">
+        <div className="">
           Home
         </div>
         </a>
--- cryptobanks-webapp/src/components/Navbar.jsx
+++ cryptobanks-webapp/src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import Crypto from '../images/crypto.png';
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center p-4 mb-12">
-      <a href='/'><div className="text-xl">Cryptobanks</div></a>
+      <a href='/'>
+        <div className="text-xl flex items-center">
+          <img className="w-12 h-auto mr-4" src={Crypto} alt="Crypto" />
+          <span>Cryptobanks</span>
+        </div>
+      </a>
       <div className="flex items-center space-x-12 mr-12">
         <div className="relative hover:text-gray-800">
           <a href='/'><span className="hover-underline">About</span></a>
