3b3dea45ac9931a27995b3e657c3c9fffc507fb6
timtg1 <tim.thorwart-gumpert@gmail.com>
2023-06-10 16:49:00 +0200

    Added linking

--- cryptobanks-webapp/src/components/index.js
+++ cryptobanks-webapp/src/components/index.js
@@ -4,3 +4,5 @@ export { default as About } from "./About";
 export { default as Introduction } from "./Introduction";
 export { default as Footer } from "./Footer";
 export { default as Introblockchain } from "./Introblockchain";
+export { default as IntegrationOverview } from "./IntegrationOverview";
+export { default as MicaIntroduction } from "./MicaIntroduction";
