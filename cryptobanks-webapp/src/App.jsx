df7c83feb678dca38eca66783832ab4f6eceb2bf
timtg1 <tim.thorwart-gumpert@gmail.com>
2023-06-18 13:32:05 +0200

    Added a Second Version of Integration Overview

--- cryptobanks-webapp/src/App.jsx
+++ cryptobanks-webapp/src/App.jsx
@@ -1,27 +1,43 @@
-import { Navbar, Hero, About, Introduction, Footer, Introblockchain, IntegrationOverview, MicaIntroduction} from "./components";
-import {BrowserRouter as Router, Route, Routes} from'react-router-dom';
+import {
+  Navbar,
+  Hero,
+  About,
+  Introduction,
+  Footer,
+  Introblockchain,
+  IntegrationOverview,
+  MicaIntroduction,
+  IntegrationOverview2,
+} from "./components";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => (
-  
+  <div>
+    <Navbar />
 
-  <div >
-    
-      <Navbar />
-      
-      <Router>
-        <Routes>
-          <Route path='/' element={<><Hero /><About /></>} />
-          <Route path='/introduction' element={<Introduction />} />
-          <Route path='/introblockchain' element={<Introblockchain />} />
-          <Route path='/integrationoverview' element={<IntegrationOverview />} />
-          <Route path='/micaintroduction' element={<MicaIntroduction />} />
-          
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <About />
+            </>
+          }
+        />
+        <Route path="/introduction" element={<Introduction />} />
+        <Route path="/introblockchain" element={<Introblockchain />} />
+        <Route path="/integrationoverview" element={<IntegrationOverview />} />
+        <Route path="/micaintroduction" element={<MicaIntroduction />} />
+        <Route
+          path="/integrationoverview2"
+          element={<IntegrationOverview2 />}
+        />
+      </Routes>
+    </Router>
 
-      <Footer />
-    
-    
+    <Footer />
   </div>
 );
 
