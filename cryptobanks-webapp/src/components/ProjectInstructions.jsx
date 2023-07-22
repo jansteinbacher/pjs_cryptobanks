0c60a86369d051d2d8911bb60f24407780db0dfc
jansteinbacher <jan.steinbacher@stud-mail.uni-wuerzburg.de>
2023-07-22 09:58:19 +0200

    popup

--- cryptobanks-webapp/src/components/ProjectInstructions.jsx
+++ cryptobanks-webapp/src/components/ProjectInstructions.jsx
@@ -1,7 +1,33 @@
 import Crypto from '../images/crypto.png';
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function ProjectInstructions() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    // Show the popup after 2 seconds
+    const timer = setTimeout(() => {
+      setShowPopup(true);
+    }, 1500);
+
+    // Clear the timer if the component is unmounted
+    return () => clearTimeout(timer);
+  }, []);
+
+  const popupRef = useRef(null);
+
+  const handleOutsideClick = (event) => {
+    // Close the popup if clicked outside of it
+    if (popupRef.current && !popupRef.current.contains(event.target)) {
+      setShowPopup(false);
+    }
+  };
+
+  const handleCloseClick = () => {
+    // Close the popup when the 'x' button is clicked
+    setShowPopup(false);
+  };
+
   return (
     <div className="flex flex-wrap">
       <div className="w-full md:w-2/3">
@@ -34,6 +60,45 @@ function ProjectInstructions() {
           className="w-1/2 h-auto"
         />
       </div>
+
+      {showPopup && (
+        <div
+          className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-gray-800 bg-opacity-50"
+          ref={popupRef}
+          onClick={handleOutsideClick}
+        >
+          <div className="relative bg-white p-8 rounded-lg">
+            <button
+              className="absolute top-2 right-2 text-gray-500"
+              onClick={handleCloseClick}
+            >
+              <span className="sr-only">Close</span>
+              <svg
+                className="w-5 h-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                ></path>
+              </svg>
+            </button>
+
+            <div class="text-center">
+              <h2 class="text-2xl font-bold">Unser Kryptokurs für Beginner</h2>
+              <p class="mt-4 text-lg">Jetzt mit unserem Kurs in die Welt der Kryptoassets, Tokens und Coins einsteigen!</p>
+              <a href="/course-beginner" class="inline-block mt-6 px-6 py-3 text-white bg-blue-500 rounded-lg hover:bg-blue-600">Zum Beginnerkurs
+              -&gt;</a>
+            </div>
+
+
+          </div>
+        </div>
+      )}
     </div>
   );
 }
@@ -45,7 +110,7 @@ function App() {
       <ProjectInstructions />
       <div className='flex items-center justify-center mt-12'>
         <hr className="w-3/5 border-t-2 border-gray-400 my-8" />
-        </div>
+      </div>
     </div>
   );
 }
