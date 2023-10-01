b5a2c5cc3669ced3e2d006020d7767caefb23a6c
jansteinbacher <jan.steinbacher@stud-mail.uni-wuerzburg.de>
2023-10-01 18:11:15 +0200

    minor changes added

---
+++ src/components/LearningUnit/RegulatoryCryptoCustody/RegulatoryCryptoCustodyPart2.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import Anwendungsbereiche from '../../../images/courseExpert/overviewAnwendungsbereiche.png';
+
+
+
+const Sources = ({ sources }) => {
+  const [showSources, setShowSources] = useState(false);
+
+  const toggleSources = () => {
+    setShowSources(!showSources);
+  };
+
+  return (
+    <div className="sources border border-gray-400 rounded p-4">
+      <button
+        className="text-lg font-semibold"
+        id="sources"
+        onClick={toggleSources}
+      >
+        Quellen:
+      </button>
+      {showSources && (
+        <div className="source-list leading-relaxed mt-4">
+          <ul>
+            {sources.map((source, index) => (
+              <li id={index + 1} key={index}>
+                [{index + 1}] {source}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const RegulatoryCryptoCustodyPart1 = () => {
+  const articlesources = [
+    "vgl. Jaroucheh, Zakwan und Baraq Ghaleb (2023). Crypto Assets Custody: Taxonomy, Components, and Open Challenges. In: 2023 IEEE International Conference on Blockchain and Cryptocurrency (ICBC), S. 1-6.",
+"vgl. Di Nicola, Vincenzo, Riccardo Longo, Federico Mazzone und Gaetano Russo (2020). Resilient Custody of Crypto-Assets, and Threshold Multisignatures. In: Mathematics 8.10, S. 1773.",
+"ebd.",
+"vgl. Jaroucheh, Zakwan und Baraq Ghaleb (2023). Crypto Assets Custody: Taxonomy, Components, and Open Challenges. In: 2023 IEEE International Conference on Blockchain and Cryptocurrency (ICBC), S. 1-6.",
+"vgl. Jaroucheh, Zakwan und Baraq Ghaleb (2023). Crypto Assets Custody: Taxonomy, Components, and Open Challenges. In: 2023 IEEE International Conference on Blockchain and Cryptocurrency (ICBC), S. 1-6.",
+"vgl. Di Nicola, Vincenzo, Riccardo Longo, Federico Mazzone und Gaetano Russo (2020). Resilient Custody of Crypto-Assets, and Threshold Multisignatures. In: Mathematics 8.10, S. 1773.",
+"vgl. Jaroucheh, Zakwan und Baraq Ghaleb (2023). Crypto Assets Custody: Taxonomy, Components, and Open Challenges. In: 2023 IEEE International Conference on Blockchain and Cryptocurrency (ICBC), S. 1-6.",
+"ebd.",
+"vgl. Suratkar, Saurabh, Mahesh Shirole und Sunil Bhirud (2023). Cryptocurrency Wallet: A Reviews. In: 2020 4th International Conference on Computer, Communication and Signal Processing (ICCCSP), S. 1-7.",
+"ebd.",
+"vgl. Jaroucheh, Zakwan und Baraq Ghaleb (2023). Crypto Assets Custody: Taxonomy, Components, and Open Challenges. In: 2023 IEEE International Conference on Blockchain and Cryptocurrency (ICBC), S. 1-6.",
+"vgl. Suratkar, Saurabh, Mahesh Shirole und Sunil Bhirud (2023). Cryptocurrency Wallet: A Reviews. In: 2020 4th International Conference on Computer, Communication and Signal Processing (ICCCSP), S. 1-7.",
+"vgl. Cabrera-Gutierrez, Antonio J., Encarnacion Castillo, Antonio Escobar-Molero, Jose A. Alvarez-Bermejo, Diego P. Morales und Luis Parrilla (2022). Integration of Hardware Security Modules and Permissioned Blockchain in Industrial IoT Networks. In: IEEE access, 2022 10, S. 114331-114345",
+"vgl. Rezaeighaleh, Hossein und Cliff C. Zou (2019). Integration of Hardware Security Modules and Permissioned Blockchain in Industrial IoT Networks. In: 2019 IEEE Global Communications Conference (GLOBECOM)",
+"vgl. A. Shamir (1979). How to share a secret. In: Communications of the ACM 22.11, S. 612–613",
+"vgl. Han, Jongbeen, Mansub Song; Hyeonsang Eom, Yongseok Son (2021). An efficient multi-signature wallet in blockchain using bloom filter. In: Proceedings of the 36th Annual ACM Symposium on Applied Computing, S. 273–281",
+"vgl. Ricci, S., Dzurenda, P., Casanova-Marqués, R., Cika, P. Marrella, A., ua. (2022). Threshold Signature for Privacy-Preserving Blockchain. In:  Business Process Management: Blockchain, Robotic Process Automation, and Central and Eastern Europe Forum. BPM 2022. Lecture Notes in Business Information Processing 459. Springer, Cham.",
+  ]
+
+  const isComingFromPreviousPage = window.location.search.includes(
+    'from=expert-course'
+  );
+
+  return (
+    <div className="container mx-auto py-12 px-12 max-w-screen-lg">
+      <h1 className="text-2xl md:text-4xl font-bold text-gray-800 mb-12 tracking-tight">
+      Anwedungsbereiche - Arten von Verwahrlösungen
+      </h1>
+
+      {isComingFromPreviousPage && (
+        <div className="p-4 mb-12 bg-green-200 rounded-lg">
+          <p className=" italic text-justify">
+          Simone hat Tobias beauftragt, sich intensiver mit verschiedenen Lösungen zur Verwahrung von Kryptoassets 
+          auseinanderzusetzen. Dies bildet den Auftakt zur Integration von Kryptoassets in das hauseigene Bankensystem. 
+          Um einen ersten Einblick in die Kryptoasset-Verwahrung zu gewinnen, untersucht Tobias, die Verwahrlösung, 
+          welche er privat zur Investition in Kryptoassets nutzt. Hierbei macht er Gebrauch von seinem intelligenten 
+          Browser-Plugin, das es ihm ermöglicht, mit der Maus über Webseiteninhalte zu fahren und weitere Informationen 
+          abzurufen.
+          </p>
+        </div>
+      )}
+
+      <p className="mb-8">
+      Die sichere Verwahrung von Kryptoassets bildet einen essenziellen Grundpfeiler für die sich entwickelnde 
+      Kryptoasset-Wirtschaft. Ganz gleich, ob es sich um Kryptowährungen, NFTs oder Security Tokens handelt – 
+      die Gewährleistung einer sicheren Aufbewahrung des privaten Schlüssels sowie die authentifizierte Genehmigung 
+      und Signierung von Transaktionen sind grundlegend. Die Kryptoasset-Verwahrlösungen können in mehrerer Klassen 
+      eingeteilt werden, basierend auf fünf Schlüsseldimensionen oder Perspektiven. <a href="#sources">[1]</a> Im Folgenden werden die einzelnen
+       Klassen näher beleuchtet.
+        
+      </p>
+
+
+      <div className="flex justify-center items-center mb-8">
+        <img
+          src={Anwendungsbereiche}
+          alt="Anwendungsbereiche"
+          className="w-full md:w-1/2"
+        />
+      </div>
+      
+
+      
+
+      
+
+      <Sources sources={articlesources} />
+    </div>
+  );
+};
+
+export default RegulatoryCryptoCustodyPart1;
