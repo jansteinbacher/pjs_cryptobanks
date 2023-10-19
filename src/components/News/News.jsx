c852884ba42c0f80980b73eb6fd730c25bf81e19
jansteinbacher <jan.steinbacher@stud-mail.uni-wuerzburg.de>
2023-10-19 21:34:17 +0200

    eslint fix news

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -12,7 +12,7 @@ function News() {
     )
       .then((response) => response.json())
       .then((data) => setArticles(data.articles))
-      
+      .catch((error) => console.error('Error fetching news:', error));
   }, [query]);
 
   return (
