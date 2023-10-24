8fa5fe83e3fdaf5a0856acc7ee42b6838f75e836
timtg1 <tim.thorwart-gumpert@gmail.com>
2023-10-24 22:04:49 +0200

    Added QueryButton

---
+++ src/components/News/QueryButton.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+
+function QueryButton({ query, setQuery, text, activeQuery }) {
+  const isActive = query === activeQuery;
+  const buttonClass = isActive
+    ? 'bg-green-500 text-white'
+    : 'bg-gray-300 text-gray-700';
+
+  const handleClick = () => {
+    if (!isActive) {
+      setQuery(activeQuery);
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      className={`px-4 py-2 rounded-lg font-semibold ${buttonClass}`}
+      onClick={handleClick}
+    >
+      {text}
+    </button>
+  );
+}
+
+QueryButton.propTypes = {
+  query: PropTypes.string.isRequired,
+  setQuery: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+  activeQuery: PropTypes.string.isRequired,
+};
+
+export default QueryButton;
