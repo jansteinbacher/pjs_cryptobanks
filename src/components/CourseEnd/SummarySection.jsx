b84c3ad879374c57ecce5e8495b25378ed975af8
jansteinbacher <jan.steinbacher@stud-mail.uni-wuerzburg.de>
2023-10-26 13:25:24 +0200

    fix downloads

--- src/components/CourseEnd/SummarySection.jsx
+++ src/components/CourseEnd/SummarySection.jsx
@@ -3,30 +3,44 @@ import { PiCertificate } from 'react-icons/pi';
 import PropTypes from 'prop-types';
 
 function SummarySection({ summaryDownloadLink, certificateDownloadLink }) {
+  const openSummary = () => {
+    // Construct the full URL to your PDF file
+    const fullPdfUrl = summaryDownloadLink;
+
+    // Open the PDF in a new tab
+    window.open(fullPdfUrl, '_blank');
+  };
+  const openCertificate = () => {
+    // Construct the full URL to your PDF file
+    const fullPdfUrl = certificateDownloadLink;
+
+    // Open the PDF in a new tab
+    window.open(fullPdfUrl, '_blank');
+  };
   return (
     <div className="lg:w-1/2">
       <div className="text-center">
         <div className="flex flex-col items-center">
           <FaFilePdf className="text-5xl text-green-400 mb-2" />
-          <a
-            href={summaryDownloadLink}
+          <button
+            type="button"
+            onClick={openSummary}
             className="inline-block px-4 py-2 bg-green-400 text-white rounded-lg hover:bg-green-500 transition duration-300"
-            download
           >
             Zusammenfassung herunterladen
-          </a>
+          </button>
         </div>
       </div>
       <div className="text-center mt-8">
         <div className="flex flex-col items-center">
           <PiCertificate className="text-5xl text-green-400 mb-2" />
-          <a
-            href={certificateDownloadLink}
+          <button
+            type="button"
+            onClick={openCertificate}
             className="inline-block px-4 py-2 bg-green-400 text-white rounded-lg hover:bg-green-500 transition duration-300"
-            download
           >
             Zertifikat herunterladen
-          </a>
+          </button>
         </div>
       </div>
     </div>
